4eb2a86a3f9104a3eb43191864d9018fdcbe68a1
MetisWastaken <luvi05@icloud.com>
2025-12-03 12:04:57 +0100

    Aggiunti SQL pratica_svolta e pratica

--- bdd/PostgreSQL/CreaPontePratica_Svolta.sql
+++ bdd/PostgreSQL/CreaPontePratica_Svolta.sql
@@ -1,1 +1,10 @@
--- Active: 1759330605908@@127.0.0.1@5433@postgres@public
+DROP TABLE IF EXISTS pratica_svolta CASCADE;
+CREATE TABLE pratica_svolta
+(
+    pratica_id INT NOT NULL,
+    ricetta_id INT NOT NULL,
+    
+    PRIMARY KEY (pratica_id, ricetta_id),
+        FOREIGN KEY(pratica_id) REFERENCES pratica(id_pratica) ON DELETE CASCADE,
+        FOREIGN KEY(ricetta_id) REFERENCES ricetta(id_ricetta) ON DELETE CASCADE
+);
--- bdd/PostgreSQL/CreaTabellaPratica.sql
+++ bdd/PostgreSQL/CreaTabellaPratica.sql
@@ -1,1 +1,13 @@
--- Active: 1759330605908@@127.0.0.1@5433@postgres@public
+DROP TABLE IF EXISTS pratica CASCADE;;
+CREATE TABLE pratica (
+    id_pratica SERIAL PRIMARY KEY,
+    giorno_sessione DATE NOT NULL,
+    aula VARCHAR(50),
+    posti_totali INTEGER,
+    corso_id INTEGER NOT NULL,
+
+    CONSTRAINT fk_corso
+        FOREIGN KEY(corso_id)
+            REFERENCES corso(id_corso) ON DELETE CASCADE
+);
+COMMENT ON TABLE pratica IS 'Tabella nel quale si memorizzano i dati relativi alle sessioni pratiche dei corsi';
