d17b49cb280d6829a62b2afac54d92cbfd4bda14
MetisWastaken <luvi05@icloud.com>
2025-12-06 17:20:18 +0100

    correzioni

--- bdd/PostgreSQL/funzioni/FunTabellaCorso.sql
+++ bdd/PostgreSQL/funzioni/FunTabellaCorso.sql
@@ -36,7 +36,7 @@ CREATE OR REPLACE FUNCTION enforce_unique_nome_corso_attivo()
 RETURNS TRIGGER AS $$
 BEGIN
     PERFORM 1 FROM corso 
-    WHERE nome = NEW.nome AND data_fin >= CURRENT_DATE);
+    WHERE nome = NEW.nome AND data_fin >= NEW.data_in;
     
     IF FOUND THEN
         RAISE EXCEPTION 'Esiste già un corso attivo con nome "%". Il nome può essere riutilizzato solo dopo il completamento del corso precedente.', NEW.nome;
