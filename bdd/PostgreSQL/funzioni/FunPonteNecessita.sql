435dacbbd6c89b80e631da3365f8ff4431ab2dbb
eppj <peppecuri1@gmail.com>
2025-12-06 10:37:50 +0100

    positive quant_ing
    aggiunta una verifica e un trigger che controlla che ogni inserimento o update porta ad avere una quantità positiva del ingrediente

--- bdd/PostgreSQL/funzioni/FunPonteNecessita.sql
+++ bdd/PostgreSQL/funzioni/FunPonteNecessita.sql
@@ -24,3 +24,17 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER trg_enforce_necessita_checks
 BEFORE INSERT OR UPDATE ON necessita
 FOR EACH ROW EXECUTE FUNCTION enforce_necessita_checks();
+
+--Trigger che la quantità dell'ingrediente sia sempre positiva
+CREATE OR REPLACE FUNCTION enforce_positive_quant_ing()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.quant_ing <= 0 THEN
+        RAISE EXCEPTION 'La quantità dell''ingrediente deve essere positiva. Valore fornito: %', NEW.quant_ing;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+CREATE TRIGGER trg_enforce_positive_quant_ing
+BEFORE INSERT OR UPDATE ON necessita
+FOR EACH ROW EXECUTE FUNCTION enforce_positive_quant_ing();
