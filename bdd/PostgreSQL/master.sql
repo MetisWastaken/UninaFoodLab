a5c6e89510ffc72da3b979b00d5a2ca6f8923f72
eppj <peppecuri1@gmail.com>
2026-01-18 17:21:05 +0100

    Updated Master.sql

--- bdd/PostgreSQL/master.sql
+++ bdd/PostgreSQL/master.sql
@@ -1,18 +1,5 @@
--- Usa questo comando da shell per creare l'intero database UninaFoodLab:
--- & "C:\Program Files\PostgreSQL\17\bin\psql.exe" -U postgres -d uninafoodlab -f "(posizione_del_file)\master.sql"
+-- CREAZIONE TABELLE
 
--- ============================================
--- MASTER SCRIPT - UninaFoodLab Database
--- ============================================
--- Ordine di esecuzione:
--- 1. Tabelle
--- 2. Ponti (relazioni)
--- 3. Funzioni
--- 4. Inserimenti di esempio
-
--- ============================================
--- 1. CREAZIONE TABELLE
--- ============================================
 \i 'CreaTabellaUtente.sql'
 \i 'CreaTabellaCorso.sql'
 \i 'CreaTabellaIngrediente.sql'
@@ -20,18 +7,16 @@
 \i 'CreaTabellaPratica.sql'
 \i 'CreaTabellaOnline.sql'
 
--- ============================================
--- 2. CREAZIONE PONTI (Relazioni)
--- ============================================
+-- CREAZIONE PONTI (Relazioni)
+
 \i 'CreaPonteIscritto_C.sql'
 \i 'CreaPonteIscritto_P.sql'
 \i 'CreaPonteTeoria.sql'
 \i 'CreaPontePratica_Svolta.sql'
 \i 'CreaPonteNecessita.sql'
 
--- ============================================
--- 3. CREAZIONE FUNZIONI
--- ============================================
+-- CREAZIONE FUNZIONI
+
 \i 'funzioni/FunTabellaUtente.sql'
 \i 'funzioni/FunTabellaCorso.sql'
 \i 'funzioni/FunTabellaIngrediente.sql'
@@ -45,14 +30,12 @@
 \i 'funzioni/FunPonteNecessita.sql'
 \i 'funzioni/View.sql'
 
--- ============================================
--- 3. INSERIMENTI DI ESEMPIO
--- ============================================
+
+-- INSERIMENTI DI ESEMPIO
+
 \i 'insert/1InserisciUtente.sql'
 \i 'insert/2InserisciCorso.sql'
 \i 'insert/3InserisciRicetta.sql'
 \i 'insert/4InserisciOnline.sql'
 \i 'insert/5InserisciPratica.sql'
--- ============================================
--- Setup completato!
--- ============================================
+
