74acbeb61fc28dffa767dc16fcc98efbc4d38eb8
eppj <peppecuri1@gmail.com>
2026-01-20 16:48:28 +0100

    update enforce_username_is_unique

--- bdd/PostgreSQL/funzioni/FunTabellaUtente.sql
+++ bdd/PostgreSQL/funzioni/FunTabellaUtente.sql
@@ -1,24 +1,17 @@
---creazione funzione e trigger per garantire che l'username sia univoco e tipo_utente sia valido
-CREATE OR REPLACE FUNCTION enforce_username_is_unique_and_valid_type()
+--creazione funzione e trigger per garantire che l'username sia univoco
+CREATE OR REPLACE FUNCTION enforce_username_is_unique()
 RETURNS TRIGGER AS $$
 BEGIN
-    IF NEW.username IS NOT NULL THEN
-        PERFORM 1 FROM utente WHERE username = NEW.username AND username <> NEW.username;
-        IF FOUND THEN
-            RAISE EXCEPTION 'Un utente con username "%" esiste gia''', NEW.username;
-        END IF;
+    IF EXISTS (SELECT 1 FROM utente WHERE username = NEW.username AND username <> OLD.username) THEN
+        RAISE EXCEPTION 'Un utente con username "%" esiste gia''', NEW.username;
     END IF;
-
-    IF NEW.tipo_utente NOT IN ('Chef', 'Studente') THEN
-        RAISE EXCEPTION 'Tipo utente "%" non valido. Deve essere "Chef" o "Studente"', NEW.tipo_utente;
-    END IF;
-
     RETURN NEW;
-END;$$ LANGUAGE plpgsql;
+END;
+$$ LANGUAGE plpgsql;
 
-CREATE TRIGGER trg_username_is_unique_and_valid_type
+CREATE TRIGGER trg_username_is_unique
 BEFORE INSERT OR UPDATE ON utente
-FOR EACH ROW EXECUTE FUNCTION enforce_username_is_unique_and_valid_type();
+FOR EACH ROW EXECUTE FUNCTION enforce_username_is_unique();
 
 --Creazione funzione e trigger per controllare la forza della password
 CREATE OR REPLACE FUNCTION password_is_strong()
