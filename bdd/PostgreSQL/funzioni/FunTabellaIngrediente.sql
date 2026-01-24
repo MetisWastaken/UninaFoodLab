c68260826b4b6642a771b83624faec634f7df270
eppj <peppecuri1@gmail.com>
2026-01-24 23:05:37 +0100

    Deleted Bugged Triggers

--- bdd/PostgreSQL/funzioni/FunTabellaIngrediente.sql
+++ bdd/PostgreSQL/funzioni/FunTabellaIngrediente.sql
@@ -1,18 +1,3 @@
---trigger che controlla che il nome dell'ingrediente sia univoco
-CREATE OR REPLACE FUNCTION check_ingrediente_nome_univoco()
-RETURNS TRIGGER AS $$
-BEGIN
-    IF EXISTS (SELECT 1 FROM ingrediente WHERE nome = NEW.nome AND nome <> OLD.nome) THEN
-        RAISE EXCEPTION 'Errore: esiste gia'' un ingrediente con il nome %', NEW.nome;
-    END IF;
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER trg_check_ingrediente_nome_univoco
-BEFORE INSERT OR UPDATE ON ingrediente
-FOR EACH ROW EXECUTE FUNCTION check_ingrediente_nome_univoco();
-
 --trigger che assicura che il nome dell'ingrediente sia valido (non numeri,simboli,spazi o vuoto)
 CREATE OR REPLACE FUNCTION check_ingrediente_nome_valido()
 RETURNS TRIGGER AS $$
--- bdd/PostgreSQL/funzioni/FunTabellaUtente.sql
+++ bdd/PostgreSQL/funzioni/FunTabellaUtente.sql
@@ -1,18 +1,3 @@
---creazione funzione e trigger per garantire che l'username sia univoco
-CREATE OR REPLACE FUNCTION enforce_username_is_unique()
-RETURNS TRIGGER AS $$
-BEGIN
-    IF EXISTS (SELECT 1 FROM utente WHERE username = NEW.username AND username <> OLD.username) THEN
-        RAISE EXCEPTION 'Un utente con username "%" esiste gia''', NEW.username;
-    END IF;
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER trg_username_is_unique
-BEFORE INSERT OR UPDATE ON utente
-FOR EACH ROW EXECUTE FUNCTION enforce_username_is_unique();
-
 --Creazione funzione e trigger per controllare la forza della password
 CREATE OR REPLACE FUNCTION password_is_strong()
 RETURNS TRIGGER AS $$
