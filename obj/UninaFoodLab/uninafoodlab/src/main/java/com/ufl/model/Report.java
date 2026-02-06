b60a1356588222dbda7def710ab79cdb19bf50ec
MetisWastaken <luvi05@icloud.com>
2026-02-06 13:05:50 +0100

    modifiche su report

--- obj/UninaFoodLab/uninafoodlab/src/main/java/com/ufl/model/Report.java
+++ obj/UninaFoodLab/uninafoodlab/src/main/java/com/ufl/model/Report.java
@@ -1,5 +1,4 @@
 package com.ufl.model;
-import java.util.HashMap;
 import java.util.Map;
 
 public class Report {
@@ -9,18 +8,7 @@ public class Report {
     private int numero_sessioni_pratiche;
     private Map<Sessione, Integer> numero_ricette_per_sessione;
 
-//costruttore default (inizializza a 0)
-    public Report() {
-        this(
-            0,
-            0,
-            0,
-            new HashMap<>()
-        );
-    }
-
-    public Report(int numero_corsi_totali, int numero_sessioni_online, int numero_sessioni_pratiche,
-            Map<Sessione, Integer> numero_ricette_per_sessione) {
+    public Report(int numero_corsi_totali, int numero_sessioni_online, int numero_sessioni_pratiche, Map<Sessione, Integer> numero_ricette_per_sessione) {
         this.numero_corsi_totali = numero_corsi_totali;
         this.numero_sessioni_online = numero_sessioni_online;
         this.numero_sessioni_pratiche = numero_sessioni_pratiche;
@@ -50,6 +38,6 @@ public class Report {
     }
 
     public Map<Sessione, Integer> getNumeroRicettePerSessione() {
-        return new HashMap<>(numero_ricette_per_sessione);
+        return new Map<>(numero_ricette_per_sessione);
     }
 }
