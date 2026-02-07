10db05567aa0f367566970cc460d26af725422f0
eppj <peppecuri1@gmail.com>
2026-02-07 20:25:25 +0100

    Created Pratica

---
+++ obj/UninaFoodLab/uninafoodlab/src/main/java/com/ufl/model/Pratica.java
@@ -0,0 +1,33 @@
+package com.ufl.model;
+
+import java.lang.reflect.Array;
+import java.time.LocalDate;
+
+public class Pratica extends Sessione {
+    private String aula;
+    private int posti_totali;
+
+    public Pratica(Integer corso_id, LocalDate giorno_sessione, ArrayList<Ricetta> ricette, String aula, int posti_totali) {
+        super(corso_id, giorno_sessione, ricette);
+        this.aula = aula;
+        this.posti_totali = posti_totali;
+    }
+
+    public String getAula() {
+        return aula;
+    }
+
+    public int getPostiTotali() {
+        return posti_totali;
+    }
+
+    public int getNStudentiIscritti() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public List<Ingrediente> getIngredientiUsati(){
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
