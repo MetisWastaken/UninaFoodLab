858345f9db47b7f05725faf6347a65bf78160c11
eppj <peppecuri1@gmail.com>
2026-02-09 11:08:36 +0100

    Update Online

--- obj/UninaFoodLab/uninafoodlab/src/main/java/com/ufl/model/Online.java
+++ obj/UninaFoodLab/uninafoodlab/src/main/java/com/ufl/model/Online.java
@@ -6,8 +6,8 @@ import java.util.ArrayList;
 public class Online extends Sessione {
     private String codice_meeting;
 
-    public Online(int corso_id, LocalDate giorno_sessione, String codice_meeting) {
-        super(corso_id, giorno_sessione);
+    public Online(int corso_id, LocalDate giorno_sessione, ArrayList<Ricetta> ricette, String codice_meeting) {
+        super(corso_id, giorno_sessione, ricette);
         this.codice_meeting = codice_meeting;
     }
 
