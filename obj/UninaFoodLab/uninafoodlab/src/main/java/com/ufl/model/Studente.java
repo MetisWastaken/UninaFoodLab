9e25ca8d48fe52d88cd2818f84165e29bcc69526
MetisWastaken <luvi05@icloud.com>
2026-02-11 17:42:51 +0100

    Studente e DAO relativo aggiornati

--- obj/UninaFoodLab/uninafoodlab/src/main/java/com/ufl/model/Studente.java
+++ obj/UninaFoodLab/uninafoodlab/src/main/java/com/ufl/model/Studente.java
@@ -2,20 +2,19 @@ package com.ufl.model;
 
 import java.time.LocalDate;
 import java.util.Map;
-import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
 
-public class Studente extends Utente {
-    private Map<Integer,LocalDate> date_iscrizione_corsi;
+import com.ufl.dao.StudenteDAO;
 
-    public Studente(String username, String password, Map<Integer, LocalDate> date_iscrizione_corsi) {
+public class Studente extends Utente {
+    private Map<Integer,LocalDate> date_iscrizione_corsi=null;
+
+    public Studente(String username, String password) {
         super(username, password, "Studente");
-        this.date_iscrizione_corsi = date_iscrizione_corsi;
     }
     public Studente(String username, String password, String nome, String cognome) {
         super(username, password, nome, cognome, "Studente");
-        this.date_iscrizione_corsi = new HashMap<>();
     }
 
     @Override
@@ -32,12 +31,15 @@ public class Studente extends Utente {
 
     @Override
     public ArrayList<Corso> getCorsi() {
-        // TODO Auto-generated method stub
-        return null;
+        return StudenteDAO.getCorsi(this.username);
     }
 
     public void iscrittoCorso(int id_corso) {
         date_iscrizione_corsi.put(id_corso, LocalDate.now());
+    }
+
+    public void recIscrizioneCorsi(){
+        //TODO Auto-generated method stub
     }
 
     public Map<Integer, LocalDate> getDateIscrizioneCorsi() {
@@ -47,6 +49,7 @@ public class Studente extends Utente {
     public void iscrittoPratica(Integer id_corso, LocalDate giorno_sessione) {
         // TODO Auto-generated method stub
     }
+    
     //metodo da modificare o controllare in futuro
     public List<Pratica> getPratiche() {
         // TODO Auto-generated method stub
