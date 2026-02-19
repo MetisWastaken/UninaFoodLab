0e34c906c0b4cdaed47eb413b193fdb61a752d16
eppj <peppecuri1@gmail.com>
2026-02-19 23:49:07 +0100

    Update Sessione.java

--- obj/UninaFoodLab/uninafoodlab/src/main/java/com/ufl/model/Sessione.java
+++ obj/UninaFoodLab/uninafoodlab/src/main/java/com/ufl/model/Sessione.java
@@ -5,12 +5,11 @@ import java.util.ArrayList;
 public abstract class Sessione {
     protected Integer corso_id;
     protected LocalDate giorno_sessione;
-    protected ArrayList<Ricetta> ricette;
+    protected ArrayList<Ricetta> ricette= null;
 
-    public Sessione(Integer corso_id, LocalDate giorno_sessione, ArrayList<Ricetta> ricette) {
+    public Sessione(Integer corso_id, LocalDate giorno_sessione) {
         this.corso_id = corso_id;
         this.giorno_sessione = giorno_sessione;
-        this.ricette = ricette;
     }
 
     public Integer getCorsoId() {
@@ -20,16 +19,17 @@ public abstract class Sessione {
     public LocalDate getGiornoSessione() {
         return giorno_sessione;
     }
-
+    abstract public void recRicette();
+    
     public ArrayList<Ricetta> getRicette() {
         return ricette;
     }
 
     public void aggiungiRicetta(Ricetta ricetta){
          // TODO Auto-generated method stub
-     }
+    }
 
      public void cancellaRicetta(Ricetta ricetta){
          // TODO Auto-generated method stub
-     }
+    }
 }
