d256bf8f49e3c83ad3456d82665586e8ead0a16e
eppj <peppecuri1@gmail.com>
2026-02-19 23:48:51 +0100

    Update Online.java

--- obj/UninaFoodLab/uninafoodlab/src/main/java/com/ufl/model/Online.java
+++ obj/UninaFoodLab/uninafoodlab/src/main/java/com/ufl/model/Online.java
@@ -7,11 +7,12 @@ public class Online extends Sessione {
     private String codice_meeting;
 
     public Online(int corso_id, LocalDate giorno_sessione, ArrayList<Ricetta> ricette, String codice_meeting) {
-        super(corso_id, giorno_sessione, ricette);
+        super(corso_id, giorno_sessione);
         this.codice_meeting = codice_meeting;
     }
     
-        public void recRicette(){
+    @Override
+    public void recRicette(){
         // This method should retrieve the meeting code for the online session.
         // Implementation depends on how meeting codes are stored and associated with the session.
     }
