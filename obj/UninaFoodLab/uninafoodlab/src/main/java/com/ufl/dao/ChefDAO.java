26f44126c0755895a69eff38a8a7de8eb05eef9c
eppj <peppecuri1@gmail.com>
2026-02-21 00:14:31 +0100

    Revert "Creazione e modifiche DAO"
    This reverts commit 12973adb9b0b55e6738c051b2773a69159fa2f49.

--- obj/UninaFoodLab/uninafoodlab/src/main/java/com/ufl/dao/ChefDAO.java
+++ obj/UninaFoodLab/uninafoodlab/src/main/java/com/ufl/dao/ChefDAO.java
@@ -4,10 +4,13 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-
+import java.util.ArrayList;
+import java.util.List;
 
 import com.ufl.model.Chef;
-
+import com.ufl.model.Corso;
+import com.ufl.model.Notifica;
+import com.ufl.model.Studente;
 
 public class ChefDAO extends ConnessioneDAO {
 
@@ -38,4 +41,62 @@ public class ChefDAO extends ConnessioneDAO {
         return null;
     }
 
+    public static List<Notifica> getNotifiche(Chef chef){
+        String query="SELECT * FROM notifica WHERE username_chef=?";
+        List<Notifica> notifiche = new ArrayList<>();
+
+        try{
+            PreparedStatement ps=connessione.prepareStatement(query);
+            ps.setString(1, chef.getUsername());
+
+            ResultSet rs=ps.executeQuery();
+
+            while(rs.next()){
+                if(rs.getBoolean("solo_iscritti") == false){
+                    notifiche.add(new Notifica(
+                        rs.getString("username_chef"),
+                        rs.getString("titolo"),
+                        rs.getString("messaggio"),
+                        rs.getDate("data_creazione").toLocalDate()
+                    ));
+                }
+                else{
+                    notifiche.add(new Notifica(
+                        rs.getString("username_chef"),
+                        rs.getString("titolo"),
+                        rs.getString("messaggio"),
+                        rs.getDate("data_creazione").toLocalDate(),
+                        (Integer) rs.getObject("corso_id")
+                    ));
+                }
+            }
+        }catch(SQLException e){
+            e.printStackTrace();            
+        }
+        return notifiche;
+    }
+
+    //public static  ArrayList<Corso> getCorsi()
+    //public static void sggiungiReport(Report report)
+    public static void aggiungiNotifica(Notifica notifica){
+        String query="INSERT INTO notifica (username_chef, titolo, messaggio, data_creazione, solo_iscritti, corso_id) VALUES (?, ?, ?, ?, ?, ?)";
+
+        try{
+            PreparedStatement ps=connessione.prepareStatement(query);
+            ps.setString(1, notifica.getUsernameChef());
+            ps.setString(2, notifica.getTitolo());
+            ps.setString(3, notifica.getMessaggio());
+            ps.setDate(4, java.sql.Date.valueOf(notifica.getDataCreazione()));
+            ps.setBoolean(5, notifica.isSoloIscritti());
+            if(notifica.isSoloIscritti()){
+                ps.setObject(6, notifica.getCorsoId(), java.sql.Types.INTEGER);
+            } else {
+                ps.setNull(6, java.sql.Types.INTEGER);
+            }
+
+            ps.executeUpdate();
+        }catch(SQLException e){
+            e.printStackTrace();            
+        }
+    }
 }
--- obj/UninaFoodLab/uninafoodlab/src/main/java/com/ufl/dao/StudenteDAO.java
+++ obj/UninaFoodLab/uninafoodlab/src/main/java/com/ufl/dao/StudenteDAO.java
@@ -1,15 +1,11 @@
 package com.ufl.dao;
 
 import java.sql.PreparedStatement;
-import java.util.ArrayList;
-import java.util.List;
 import java.sql.SQLException;
 import java.sql.ResultSet;
-import java.time.LocalDate;
 
-import com.ufl.model.Notifica;
 import com.ufl.model.Studente;
-import com.ufl.model.Corso;
+
 
 public class StudenteDAO extends ConnessioneDAO{
 
@@ -38,63 +34,6 @@ public class StudenteDAO extends ConnessioneDAO{
             e.printStackTrace();            
         }
         return null;
-    }
-
-    public static ArrayList<Corso> getCorsi(String username){
-        
-        String query="SELECT c.id, c.nome, c.categoria, c.data_in, c.data_fin, c.frequenza_settimanale, c.id_chef FROM corso c JOIN iscrizione_corsi ic ON c.id = ic.id_corso JOIN utente u ON ic.username = u.username WHERE u.username = ?";
-        
-        ArrayList<Corso> corsi = new ArrayList<>();
-        
-        try{
-            PreparedStatement ps=connessione.prepareStatement(query);
-            ps.setString(1, username);
-            
-            ResultSet rs=ps.executeQuery();
-            
-            while(rs.next()){
-                Integer id=rs.getInt("id");
-                String nome=rs.getString("nome");
-                String categoria=rs.getString("categoria");
-                LocalDate data_in=rs.getDate("data_in").toLocalDate();
-                LocalDate data_fin=rs.getDate("data_fin").toLocalDate();
-                String frequenza_settimanale=rs.getString("frequenza_settimanale");
-
-                Corso corso = new Corso(id, nome, categoria, data_in, data_fin, frequenza_settimanale, null, null, null);
-                corsi.add(corso);
-            }
-        }catch(SQLException e){
-            e.printStackTrace();            
-        }
-        return corsi;
-        //TODO:revisione necessaria posy revisione "grafici"
-    }
-
-    public static List<Notifica> getNotifiche(String username){
-        
-        String query="SELECT * FROM view_notifiche_studente WHERE studente_username=?";
-        List<Notifica> notifiche = new ArrayList<>();
-        
-        try{
-            PreparedStatement ps=connessione.prepareStatement(query);
-            ps.setString(1, username);
-            
-            ResultSet rs=ps.executeQuery();
-            
-            while(rs.next()){
-                String username_studente=rs.getString("username_studente");
-                String titolo=rs.getString("titolo");
-                String messaggio=rs.getString("messaggio");
-                boolean solo_iscritti=rs.getBoolean("solo_iscritti");
-                int id_corso=rs.getInt("id_corso");
-
-                //TODO: gestione if/else per notifiche solo iscritti o meno
-                
-            }
-        }catch(SQLException e){
-            e.printStackTrace();            
-        }
-        return notifiche;
     }
 }
 
