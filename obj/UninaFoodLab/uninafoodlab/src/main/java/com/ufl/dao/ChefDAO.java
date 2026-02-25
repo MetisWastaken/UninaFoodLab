17af997b48a26195981e9ed683b71bd80a52a575
eppj <peppecuri1@gmail.com>
2026-02-25 23:00:04 +0100

    Update chef

--- obj/UninaFoodLab/uninafoodlab/src/main/java/com/ufl/dao/ChefDAO.java
+++ obj/UninaFoodLab/uninafoodlab/src/main/java/com/ufl/dao/ChefDAO.java
@@ -1,12 +1,17 @@
 package com.ufl.dao;
 
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-
+import java.util.List;
+import java.util.ArrayList;
 
 import com.ufl.model.Chef;
+import com.ufl.model.Notifica;
+import com.ufl.model.Corso;
+import com.ufl.model.Report;
 
 
 public class ChefDAO extends ConnessioneDAO {
@@ -38,4 +43,91 @@ public class ChefDAO extends ConnessioneDAO {
         return null;
     }
 
+    public static boolean verify(Chef chef){
+        String query="SELECT * FROM utente WHERE username=? AND password=? AND tipo_utente='Chef'";
+
+        try{
+            PreparedStatement ps=connessione.prepareStatement(query);
+            ps.setString(1, chef.getUsername());
+            ps.setString(2, chef.getPassword());
+
+            ResultSet rs=ps.executeQuery();
+            if(rs.next()){
+                String tipo = rs.getString("tipo_utente");
+                if(tipo.equals("Chef")){
+                    return true; // metodo necessario per far visualizzare all'utente l'errore in futuro
+                }
+            }
+            return false;
+
+        }catch(SQLException e){
+            e.printStackTrace();            
+        }
+        return false; 
+    }
+
+    public static List<Notifica> getNotifiche(Chef chef){
+        String query = "SELECT id_notifica FROM notifica WHERE username_chef=?";
+        List<Notifica> notifiche = new ArrayList<>();
+        
+        try{
+            PreparedStatement ps = connessione.prepareStatement(query);
+            ps.setString(1, chef.getUsername());
+            
+            ResultSet rs = ps.executeQuery();
+            
+            while(rs.next()){
+                Integer id_notifica = rs.getInt("id_notifica");
+                Notifica notifica = NotificaDAO.get(id_notifica);
+                if(notifica != null){
+                    notifiche.add(notifica);
+                }
+            }
+        }catch(SQLException e){
+            e.printStackTrace();            
+        }
+        return notifiche; 
+    }
+
+    public static ArrayList<Corso> getCorsi(Chef chef){
+        String query = "SELECT id_corso FROM corso WHERE chef_id=?";
+        ArrayList<Corso> corsi = new ArrayList<>();
+        
+        try{
+            PreparedStatement ps = connessione.prepareStatement(query);
+            ps.setString(1, chef.getUsername());
+            
+            ResultSet rs = ps.executeQuery();
+            
+            while(rs.next()){
+                Integer id_corso = rs.getInt("id_corso");
+                Corso corso = new CorsoDAO().get(id_corso);
+                if(corso != null){
+                    corsi.add(corso);
+                }
+            }
+        }catch(SQLException e){
+            e.printStackTrace();            
+        }
+        return corsi; 
+    }
+
+    public static Report recResoconto(Chef chef){
+        return ReportDAO.get(chef.getUsername());
+    }
+
+    public static void aggiungiNotifica(Chef chef, Notifica notifica){
+        // This method should add a notification for the given chef in the database.
+        // Implementation depends on how notifications are stored and associated with the chef.
+    }
+
+    public static void eliminaNotifica(Chef chef, Integer id_notifica){
+        // This method should remove a notification for the given chef from the database.
+        // Implementation depends on how notifications are stored and associated with the chef.
+    }
+
+    public static void aggiungiCorso(Chef chef, Corso corso){
+        // This method should add a course for the given chef in the database.
+        // Implementation depends on how courses are stored and associated with the chef.
+    }
 }
--- obj/UninaFoodLab/uninafoodlab/src/main/java/com/ufl/model/Chef.java
+++ obj/UninaFoodLab/uninafoodlab/src/main/java/com/ufl/model/Chef.java
@@ -3,7 +3,6 @@ package com.ufl.model;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.ufl.dao.UtenteDAO;
 import com.ufl.dao.ChefDAO;
 
 public class Chef extends Utente {
@@ -35,21 +34,13 @@ public class Chef extends Utente {
         return null;
     }
 
-    public boolean equals(Utente utente){
-        // This method should compare two Chef objects for equality.
-        // Implementation depends on which attributes should be compared.
-        return false; // Placeholder return statement
-    }
-
     public void recResoconto(){
         // This method should retrieve the report/resoconto for the chef.
         // Implementation depends on how reports are stored and associated with the chef.
     }
 
     public Report getResoconto(){
-        // This method should return the report/resoconto for the chef.
-        // Implementation depends on how reports are retrieved.
-        return resoconto; // Return the stored resoconto
+        return resoconto; 
     }
 
     public void aggiungiNotifica(Notifica notifica){
