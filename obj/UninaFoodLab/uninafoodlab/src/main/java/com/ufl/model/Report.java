c0d54aead0b6fbc5de8c4145e25ff8bbcd684b09
eppj <peppecuri1@gmail.com>
2026-03-12 11:49:02 +0100

    Modifica regole STILISTICHE Report + UML

--- obj/UninaFoodLab/uninafoodlab/src/main/java/com/ufl/model/Report.java
+++ obj/UninaFoodLab/uninafoodlab/src/main/java/com/ufl/model/Report.java
@@ -9,9 +9,9 @@ public class Report {
     private int numero_sessioni_online;
     private int numero_sessioni_pratiche;
     private Map<Pratica, Integer> numero_ricette_per_pratica = new HashMap<>();
-    private double mediaricette;
-    private int MaxRicette;
-    private int MinRicette;
+    private double media_ricette;
+    private int max_ricette;
+    private int min_ricette;
 
 
     public Report(String username_chef, int numero_corsi_totali, int numero_sessioni_online, int numero_sessioni_pratiche) {
@@ -44,16 +44,16 @@ public class Report {
         return numero_ricette_per_pratica;
     }
 
-    public double getmediaricette() {
-        return mediaricette;
+    public double getMediaRicette() {
+        return media_ricette;
     }
 
     public int getMaxRicette() {
-        return MaxRicette;
+        return max_ricette;
     }
 
     public int getMinRicette() {
-        return MinRicette;
+        return min_ricette;
     }
 
 
@@ -66,9 +66,9 @@ public class Report {
     // ---- METODI ----
     public void calcolaStatisticheRicettePratiche() {
         if (numero_ricette_per_pratica == null || numero_ricette_per_pratica.isEmpty()) {
-            mediaricette = 0.0;
-            MinRicette = 0;
-            MaxRicette = 0;
+            media_ricette = 0.0;
+            min_ricette = 0;
+            max_ricette = 0;
             return;
         }
         
@@ -86,8 +86,8 @@ public class Report {
         }
 
         int count = numero_ricette_per_pratica.size();
-        mediaricette = count == 0 ? 0.0 : (double) somma / count;
-        MinRicette = (min == Integer.MAX_VALUE) ? 0 : min;
-        MaxRicette = (max == Integer.MIN_VALUE) ? 0 : max;
+        media_ricette = count == 0 ? 0.0 : (double) somma / count;
+        min_ricette = (min == Integer.MAX_VALUE) ? 0 : min;
+        max_ricette = (max == Integer.MIN_VALUE) ? 0 : max;
     }
 }
