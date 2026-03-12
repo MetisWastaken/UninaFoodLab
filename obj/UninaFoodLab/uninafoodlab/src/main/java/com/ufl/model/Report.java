c70a219f35baeb9d94222d824e435ed380ab38f6
MetisWastaken <luvi05@icloud.com>
2026-03-12 11:21:01 +0100

    Report completo

--- obj/UninaFoodLab/uninafoodlab/src/main/java/com/ufl/model/Report.java
+++ obj/UninaFoodLab/uninafoodlab/src/main/java/com/ufl/model/Report.java
@@ -9,6 +9,10 @@ public class Report {
     private int numero_sessioni_online;
     private int numero_sessioni_pratiche;
     private Map<Pratica, Integer> numero_ricette_per_pratica = new HashMap<>();
+    private double mediaricette;
+    private int MaxRicette;
+    private int MinRicette;
+
 
     public Report(String username_chef, int numero_corsi_totali, int numero_sessioni_online, int numero_sessioni_pratiche) {
         this.username_chef = username_chef;
@@ -40,6 +44,19 @@ public class Report {
         return numero_ricette_per_pratica;
     }
 
+    public double getmediaricette() {
+        return mediaricette;
+    }
+
+    public int getMaxRicette() {
+        return MaxRicette;
+    }
+
+    public int getMinRicette() {
+        return MinRicette;
+    }
+
+
     // ---- REC ----
 
     public void recNumeroRicettePerPratiche() {
@@ -47,8 +64,30 @@ public class Report {
     }
 
     // ---- METODI ----
+    public void calcolaStatisticheRicettePratiche() {
+        if (numero_ricette_per_pratica == null || numero_ricette_per_pratica.isEmpty()) {
+            mediaricette = 0.0;
+            MinRicette = 0;
+            MaxRicette = 0;
+            return;
+        }
+        
+        int somma = 0;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
 
-    public void visualizzaGrafico() {
-        // TODO: compito della view
+        for (Integer n : numero_ricette_per_pratica.values()) {
+            if (n == null) {
+                continue;
+            }
+            somma = somma + n;
+            if (n < min) min = n;
+            if (n > max) max = n;
+        }
+
+        int count = numero_ricette_per_pratica.size();
+        mediaricette = count == 0 ? 0.0 : (double) somma / count;
+        MinRicette = (min == Integer.MAX_VALUE) ? 0 : min;
+        MaxRicette = (max == Integer.MIN_VALUE) ? 0 : max;
     }
 }
---
+++ obj/UninaFoodLab/uninafoodlab/src/main/java/com/ufl/view/VisualizzaJFreeChartReport.java
@@ -0,0 +1,117 @@
+package com.ufl.view;
+
+import com.ufl.model.Report;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.data.category.DefaultCategoryDataset;
+    
+import javax.swing.*;
+import java.awt.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+public class VisualizzaJFreeChartReport extends UiUtil.BlankPanel {
+
+    public VisualizzaJFreeChartReport(Report report) {
+
+        super(UiUtil.COLORE_SFONDO);
+
+        setLayout(new BorderLayout(10,10));
+        setBorder(BorderFactory.createEmptyBorder(16, 16, 16, 16));
+
+        String mese = LocalDate.now().getMonth().getDisplayName(TextStyle.FULL, Locale.getDefault());
+        JLabel titolo = new JLabel("Report del mese corrente: " + mese);
+        titolo.setFont((new Font(UiUtil.FONT_FAMILY, Font.BOLD, 18)));
+        titolo.setForeground(UiUtil.COLORE_TESTO1);
+
+        add(titolo, BorderLayout.NORTH);
+
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+
+        dataset.addValue(report.getNumeroCorsiTotali(), "", "Corsi Totali");
+        dataset.addValue(report.getNumeroSessioniOnline(), "", "Online");
+        dataset.addValue(report.getNumeroSessioniPratiche(), "", "Pratiche");
+        dataset.addValue(report.getmediaricette(), "", "Ricette");
+        dataset.addValue(report.getMinRicette(), "", "Min Ricette");
+        dataset.addValue(report.getMaxRicette(), "", "Max Ricette");
+
+        JFreeChart chart = ChartFactory.createBarChart(
+            "Report Mensile",
+            "",
+            "Valori",
+            dataset
+        );
+        chart.removeLegend();
+         
+        CategoryPlot plot = chart.getCategoryPlot();
+        plot.setBackgroundPaint(Color.WHITE);
+        plot.setRangeGridlinePaint(UiUtil.COLORE_ACCENTO);
+
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setPreferredSize(new Dimension(800, 600));
+
+        add(chartPanel, BorderLayout.CENTER);
+
+            JLabel footer = new JLabel(
+            "Media ricette: " + report.getmediaricette() + " | Min: " + report.getMinRicette() + " | Max: " + report.getMaxRicette()
+        
+        );
+
+        footer.setForeground(UiUtil.COLORE_TESTO2);
+
+        add(footer, BorderLayout.SOUTH);
+
+        JButton salvaBtn = UiUtil.createButton("Salva Report");
+        salvaBtn.addActionListener(e -> SalvaReport(report, mese));
+
+        add(salvaBtn, BorderLayout.SOUTH);
+
+    }
+
+    private void SalvaReport(Report report, String mese) {
+
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Salva Report");
+        fileChooser.setSelectedFile(new java.io.File("Report_" + mese + ".txt"));
+        
+        int result = fileChooser.showSaveDialog(this);
+        if (result != JFileChooser.APPROVE_OPTION) return;
+
+        java.io.File file = fileChooser.getSelectedFile();
+
+        if(!file.getName().endsWith(".txt")) {
+            file = new java.io.File(file.getPath() + ".txt");
+        }
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+
+                writer.write("### Report Mensile: " + mese + " ###");
+                writer.newLine();
+                writer.write("Corsi Totali:         " + report.getNumeroCorsiTotali());
+                writer.newLine();
+                writer.write("Sessioni Online:      " + report.getNumeroSessioniOnline());
+                writer.newLine();
+                writer.write("Sessioni Pratiche:    " + report.getNumeroSessioniPratiche());
+                writer.newLine();
+                writer.write("Media Ricette:        " + report.getmediaricette());
+                writer.newLine();
+                writer.write("Min Ricette:          " + report.getMinRicette());
+                writer.newLine();
+                writer.write("Max Ricette:          " + report.getMaxRicette());
+                writer.newLine();
+
+                JOptionPane.showMessageDialog(this, "Report salvato");
+
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(this, "Errore durante il salvataggio:");
+        }
+
+    }
+    
+}
