1d3ec6fb067ba6c082de7c502513157dfe9494e6
eppj <peppecuri1@gmail.com>
2026-03-18 19:09:17 +0100

    Update 6InserisciNotifiche.sql

--- bdd/PostgreSQL/insert/6InserisciNotifiche.sql
+++ bdd/PostgreSQL/insert/6InserisciNotifiche.sql
@@ -1,5 +1,3 @@
-
--- Notifica globale (destinata a TUTTI gli studenti)
 INSERT INTO notifica (titolo, messaggio, solo_iscritti, username_chef, corso_id) 
 VALUES (
     'Aggiornamento importante da Chef Gennaro',
@@ -7,31 +5,22 @@ VALUES (
     FALSE, 
     'GEsposito',
     NULL    
-);
-
--- Notifica solo iscritti
-INSERT INTO notifica (titolo, messaggio, solo_iscritti, username_chef, corso_id) 
-VALUES (
+),
+(
     'Avviso per gli Iscritti al corso di "Pasta Fresca"',
     'Carissimi aspiranti cuochi, vi ricordo di portare da casa il vostro grembiule personale e retina per capelli, non saranno concessi scambi di materiale.',
     TRUE, 
     'GEsposito',
     (SELECT id_corso FROM corso WHERE nome = 'Pasta Fresca')
-);
-
--- Notifica globale (destinata a TUTTI gli studenti)
-INSERT INTO notifica (titolo, messaggio, solo_iscritti, username_chef, corso_id) 
-VALUES (
+),
+(
     'Messaggio di benvenuto da Chef Anna',
     'Un caloroso benvenuto a tutti i nuovi studenti iscritti ai nostri corsi, vi ricordo di consultare regolarmente la piattaforma per rimanere aggiornati su tutte le novità e comunicazioni importanti.',
     FALSE, 
     'ASmith',
     NULL    
-);
-
--- Solo iscritti
-INSERT INTO notifica (titolo, messaggio, solo_iscritti, username_chef, corso_id) 
-VALUES (
+),
+(
     'Comunicazione di servizio corso Cucina Vegetariana',
     'Cari alunni, dopo gli ultimi accadimenti, sono tenuto a precisare che è severamente vietato utilizzare le foglie della pianta del corridoio interno come elemento decorativo dei piatti, soprattutto durante gli esami pratici con esterni.',
     TRUE,  
